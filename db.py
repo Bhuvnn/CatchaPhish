d08b63f1a73d5d941eed60a7a9cb062da7eaa621
Bhuvnn <140882654+Bhuvnn@users.noreply.github.com>
2025-02-11 11:29:01 +0530

    new

--- db.py
+++ db.py
@@ -7,8 +7,8 @@ from datetime import datetime
 cloud_config= {
   'secure_connect_bundle': r'secure-connect-phishing-db.zip'
 }
-CLIENT_ID = os.environ.get["clientId"]
-CLIENT_SECRET = os.environ.get["secret"]
+CLIENT_ID = os.environ.get("clientId")
+CLIENT_SECRET = os.environ.get("secret")
 
 auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
